afff5e86bef10238ddfc2e6dbbb9e9a6e485f27b
Roland McRae <mcraeroland@gmail.com>
2020-03-08 02:50:29 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,45 @@
+
+# Define display_board that accepts a board and prints
+# out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if (index < 0) || (index > 8)
+    return false
+  elsif position_taken?(board, index)
+    return false
+  else
+    return true
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || "O"
+    return true
+  end
+end
+
+
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+  index = input.to_i
+  index -= 1
+end
+
+def move(board, index, value = "X")
+    board[index] = value
+
+end
